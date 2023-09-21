ad5d928800814fd4d63bf988218a5a2f1dd3dcfd
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-09-21 20:35:08 +0900

    Update riverpod_examples.dart

--- lib/riverpod_examples.dart
+++ lib/riverpod_examples.dart
@@ -1,7 +1,5 @@
-import 'package:riverpod/riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'riverpod_examples.g.dart';
 
