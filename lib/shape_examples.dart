a4275f5479020b8ddbd8bb70386916757e41033c
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-10-12 21:05:46 +0900

    Update shape_examples.dart

--- lib/shape_examples.dart
+++ lib/shape_examples.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-void ovalOutlinedButtonExample() {
-  OutlinedButton(
-    onPressed: () {},
-    style: OutlinedButton.styleFrom(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(8),
+Widget ovalOutlinedButtonExample() => OutlinedButton(
+      onPressed: () {},
+      style: OutlinedButton.styleFrom(
+        backgroundColor: Colors.yellow,
+        side: const BorderSide(color: Colors.red),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(8),
+          ),
         ),
       ),
-    ),
-    child: const Text('Click me!'),
-  );
-}
+      child: const Text('Click me!'),
+    );
