1f6820dcbda1ea054881ae841595bd34f4532d3f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-10-27 23:51:35 +0900

    Update shared_preferences_wrapper2.dart

--- lib/sharedpreferences/shared_preferences_wrapper2.dart
+++ lib/sharedpreferences/shared_preferences_wrapper2.dart
@@ -2,8 +2,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesWrapper2 {
   static Future<void> init() async {
-    prefs = await SharedPreferences.getInstance();
+    instance = await SharedPreferences.getInstance();
   }
 
-  static late SharedPreferences prefs;
+  static late SharedPreferences instance;
 }
