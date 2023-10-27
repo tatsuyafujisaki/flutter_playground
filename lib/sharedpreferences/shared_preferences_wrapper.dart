3ee6a4d44d2d06cb18f3d81ee5da2fa484382f14
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-10-27 23:17:53 +0900

    Create shared_preferences_wrapper.dart

---
+++ lib/sharedpreferences/shared_preferences_wrapper.dart
@@ -0,0 +1,33 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesWrapper {
+  SharedPreferencesWrapper() {
+    Future.delayed(
+      Duration.zero,
+      () async {
+        _prefs = await SharedPreferences.getInstance();
+      },
+    );
+  }
+
+  static late SharedPreferences _prefs;
+
+  static Future<int?> getInt(String key) async => _prefs.getInt(key);
+  static Future<bool?> getBool(String key) async => _prefs.getBool(key);
+  static Future<double?> getDouble(String key) async => _prefs.getDouble(key);
+  static Future<String> getString(String key) async =>
+      _prefs.getString(key) ?? '';
+  static Future<List<String>> getStringList(String key) async =>
+      _prefs.getStringList(key) ?? [];
+
+  static Future<bool> setInt(String key, int value) =>
+      _prefs.setInt(key, value);
+  static Future<bool> setBool(String key, bool value) async =>
+      _prefs.setBool(key, value);
+  static Future<bool> setDouble(String key, double value) =>
+      _prefs.setDouble(key, value);
+  static Future<bool> setString(String key, String value) =>
+      _prefs.setString(key, value);
+  static Future<bool> setStringList(String key, List<String> value) =>
+      _prefs.setStringList(key, value);
+}
