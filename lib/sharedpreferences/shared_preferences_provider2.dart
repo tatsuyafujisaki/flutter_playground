64216c825618e7414d077dddbb9071df6ebeddd7
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-10-28 02:08:33 +0900

    Delete shared_preferences_provider2.dart

--- lib/sharedpreferences/shared_preferences_provider2.dart
+++
@@ -1,9 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class SharedPreferencesProvider2 {
-  static late SharedPreferences instance;
-
-  static Future<void> initialize() async {
-    instance = await SharedPreferences.getInstance();
-  }
-}
