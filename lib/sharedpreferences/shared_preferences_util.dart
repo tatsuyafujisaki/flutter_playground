4b5113d849dc3ea28a957ce20ba2147abc66b0c3
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-12 16:27:12 +0900

    Update shared_preferences_util.dart

--- lib/sharedpreferences/shared_preferences_util.dart
+++ lib/sharedpreferences/shared_preferences_util.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<void> dumpSharedPreferences() async {
-  final prefs = await SharedPreferences.getInstance();
-  prefs.getKeys().forEach(
-    (key) {
-      debugPrint('key=$key, value=${prefs.get(key)}');
-    },
-  );
+class SharedPreferencesUtil {
+  static Future<void> dump() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.getKeys().forEach(
+      (key) {
+        debugPrint('key=$key, value=${prefs.get(key)}');
+      },
+    );
+  }
 }
