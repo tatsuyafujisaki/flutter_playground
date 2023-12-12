05499c4e19ff33c1b40cd9778c558214a38b2543
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-12 12:01:36 +0900

    Update encypted_shared_preferences.dart

--- lib/sharedpreferences/encypted_shared_preferences.dart
+++ lib/sharedpreferences/encypted_shared_preferences.dart
@@ -8,15 +8,18 @@ class EncryptedSharedPreferences {
     _prefs = await SharedPreferences.getInstance();
   }
 
-  static String getString(String key) {
-    final encrypted = _prefs.getString(key) ?? '';
-    return encrypted.isEmpty ? '' : Encryptor.decrypt(encrypted);
-  }
+  static String getString(String key) =>
+      Encryptor.decrypt(_prefs.getString(key) ?? '');
 
   static List<String> getStringList(String key) =>
-      _prefs.getStringList(key)?.map(Encryptor.decrypt).toList() ?? [];
+      (_prefs.getStringList(key) ?? []).map(Encryptor.decrypt).toList();
 
   static Future<bool> setString(String key, String value) => value.isEmpty
       ? Future.value(false)
       : _prefs.setString(key, Encryptor.encrypt(value));
+
+  static Future<bool> setStringList(String key, List<String> value) =>
+      value.isEmpty
+          ? Future.value(false)
+          : _prefs.setStringList(key, value.map(Encryptor.encrypt).toList());
 }
