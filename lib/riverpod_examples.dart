96c1fa50529f9bf9e08e60ef1888f39013d08bae
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-19 15:07:46 +0900

    Update riverpod_examples.dart

--- lib/riverpod_examples.dart
+++ lib/riverpod_examples.dart
@@ -3,12 +3,12 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'riverpod_examples.g.dart';
 
-final myBoolStateProvider = StateProvider.autoDispose((_) => false);
+final _myBoolStateProvider = StateProvider.autoDispose((_) => false);
 
 @riverpod
 bool myGeneratedBool(MyGeneratedBoolRef ref) => false;
 
 /// https://docs-v2.riverpod.dev/docs/providers/state_provider#how-to-update-the-state-based-on-the-previous-value-without-reading-the-provider-twice
 void updateStateExample(WidgetRef ref) {
-  ref.read(myBoolStateProvider.notifier).update((state) => !state);
+  ref.read(_myBoolStateProvider.notifier).update((state) => !state);
 }
