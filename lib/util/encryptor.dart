3dbe02bf190b305b20f4cfe83b3e64ad4f96b39c
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-12-19 21:30:14 +0900

    Update encryptor.dart

--- lib/util/encryptor.dart
+++ lib/util/encryptor.dart
@@ -1,16 +1,14 @@
 import 'package:encrypt/encrypt.dart';
 
-class Encryptor {
-  static final _encryptor = Encrypter(
-    AES(Key.fromBase64('wnhXiuxpZa8Aqm5yTWPAvJ660ZKJV6rDH3eVAfKFz/U=')),
-  );
-  static final _iv = IV.fromLength(16);
+final _encryptor = Encrypter(
+  AES(Key.fromBase64('6ZE61Jel3H8bdRNvGz7BGPK2eJWYfczTZgQZRZt9knM=')),
+);
+final _iv = IV.fromLength(16);
 
-  static String encrypt(String input) =>
-      // Passing an empty string to encrypt() will throw an exception.
-      input.isEmpty ? '' : _encryptor.encrypt(input, iv: _iv).base64;
+String encrypt(String input) =>
+    // Passing an empty string to encrypt() will throw an exception.
+    input.isEmpty ? '' : _encryptor.encrypt(input, iv: _iv).base64;
 
-  static String decrypt(String encoded) =>
-      // Passing an empty string to decrypt64() will throw an exception.
-      encoded.isEmpty ? '' : _encryptor.decrypt64(encoded, iv: _iv);
-}
+String decrypt(String encoded) =>
+    // Passing an empty string to decrypt64() will throw an exception.
+    encoded.isEmpty ? '' : _encryptor.decrypt64(encoded, iv: _iv);
