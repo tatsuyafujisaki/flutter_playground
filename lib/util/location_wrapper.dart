467c31b4e7e3a006f88f960025a6915000d08717
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-01-29 14:34:41 +0900

    Update location_wrapper.dart

--- lib/util/location_wrapper.dart
+++ lib/util/location_wrapper.dart
@@ -1,37 +1,38 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
 import 'package:location/location.dart';
 
 class LocationWrapper {
   final _location = Location();
+  StreamSubscription<LocationData>? subscription;
 
   Future<bool> get _isAllowed async {
     if (!await _location.serviceEnabled() &&
         !await _location.requestService()) {
       return false;
     }
-    if (!await _location.isBackgroundModeEnabled() &&
-        !await _location.enableBackgroundMode()) {
-      return false;
-    }
     var status = await _location.hasPermission();
     if (status == PermissionStatus.denied) {
       status = await _location.requestPermission();
     }
-    if (status == PermissionStatus.denied ||
-        status == PermissionStatus.deniedForever) {
-      return false;
-    }
-    return true;
+    return {PermissionStatus.granted, PermissionStatus.grantedLimited}
+        .contains(status);
   }
 
   Future<LocationData?> get location async =>
       await _isAllowed ? _location.getLocation() : null;
 
   Future<void> listen(void Function(LocationData) onData) async {
-    if (await _isAllowed) {
-      _location.onLocationChanged.listen(onData);
+    if (!await _isAllowed) {
+      return;
     }
+    await _location.changeSettings(
+      interval: 15000,
+    );
+    await subscription?.cancel();
+    subscription = _location.onLocationChanged.listen(onData);
   }
 
   void demoOneShot() {
