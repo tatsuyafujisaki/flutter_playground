736a63e548a32edf1207e2675091a9a276ca90e1
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-08-06 23:03:50 +0900

    Update playground2.dart

--- lib/widgets/playground2.dart
+++ lib/widgets/playground2.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 
 class _MyStatelessWidget extends StatelessWidget {
-  const _MyStatelessWidget();
+  const _MyStatelessWidget({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
+  Widget build(BuildContext context) => const MaterialApp(
         home: Scaffold(
-          backgroundColor: Colors.white,
-          body: IconButton(
-            icon: const Icon(Icons.flutter_dash),
-            onPressed: () async {
-              await showModalBottomSheet<void>(
-                context: context,
-                builder: (context) => const Text('aaa'),
-                useSafeArea: true,
-              );
-            },
-          ),
+          body: BottomSheetExample(),
+        ),
+      );
+}
+
+class BottomSheetExample extends StatelessWidget {
+  const BottomSheetExample({super.key});
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: ElevatedButton(
+          child: const Text('showModalBottomSheet'),
+          onPressed: () async {
+            await showModalBottomSheet<void>(
+              context: context,
+              builder: (context) => Container(
+                height: 200,
+                color: Colors.amber,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text('Modal BottomSheet'),
+                      ElevatedButton(
+                        child: const Text('Close BottomSheet'),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       );
 }
