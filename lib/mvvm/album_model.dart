d3a88ff08de9bab7770517296127524a3af8adc6
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-08-15 13:15:36 +0900

    Update album_model.dart

--- lib/mvvm/album_model.dart
+++ lib/mvvm/album_model.dart
@@ -12,25 +12,22 @@ class AlbumModel {
   Future<List<Album>> getAlbums() async {
     final uri = Uri.https(_baseUrl, 'albums');
     final response = await http.get(uri);
-    if (response.statusCode == 200) {
-      final mapList = jsonDecode(response.body) as List<dynamic>;
-      return mapList
+    if (_isSuccessful(response.statusCode)) {
+      return (jsonDecode(response.body) as List<dynamic>)
           .map((map) => Album.fromJson(map as Map<String, dynamic>))
           .toList();
-    } else {
-      throw Exception('Failed to get albums.');
     }
+    throw Exception('Failed to get albums.');
   }
 
   /// https://docs.flutter.dev/cookbook/networking/fetch-data#2-make-a-network-request
   Future<Album> getAlbum({required int id}) async {
     final uri = Uri.https(_baseUrl, 'albums/$id');
     final response = await http.get(uri);
-    if (response.statusCode == 200) {
+    if (_isSuccessful(response.statusCode)) {
       return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-    } else {
-      throw Exception('Failed to get an album.');
     }
+    throw Exception('Failed to get an album.');
   }
 
   /// https://docs.flutter.dev/cookbook/networking/send-data#convert-the-http-response-to-an-album
@@ -43,11 +40,10 @@ class AlbumModel {
       },
       body: jsonEncode(body),
     );
-    if (response.statusCode == 201) {
+    if (_isSuccessful(response.statusCode)) {
       return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-    } else {
-      throw Exception('Failed to post an album.');
     }
+    throw Exception('Failed to post an album.');
   }
 
   /// https://docs.flutter.dev/cookbook/networking/update-data#2-updating-data-over-the-internet-using-the-http-package
@@ -63,15 +59,13 @@ class AlbumModel {
       },
       body: jsonEncode(body),
     );
-    if (response.statusCode == 200) {
+    if (_isSuccessful(response.statusCode)) {
       return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-    } else {
-      throw Exception('Failed to update an album.');
     }
+    throw Exception('Failed to update an album.');
   }
 
   /// https://docs.flutter.dev/cookbook/networking/delete-data#2-delete-data-on-the-server
-// ignore: unused_element
   Future<void> deleteAlbum({required int id}) async {
     final uri = Uri.https(_baseUrl, 'albums/$id');
     final response = await http.delete(
@@ -80,35 +74,36 @@ class AlbumModel {
         HttpHeaders.contentTypeHeader: ContentType.json.toString(),
       },
     );
-    if (response.statusCode != 200) {
+    if (!_isSuccessful(response.statusCode)) {
       throw Exception('Failed to delete an album.');
     }
   }
+
+  bool _isSuccessful(int statusCode) => 200 <= statusCode && statusCode <= 299;
 }
 
 void main() async {
   final albumModel = AlbumModel();
 
-  debugPrint((await albumModel.getAlbums()).toString());
-  debugPrint((await albumModel.getAlbum(id: 1)).toString());
-  debugPrint(
-    (await albumModel.postAlbum(
-      body: <String, dynamic>{
-        'userId': 42,
-        'title': 'My album',
-      },
-    ))
-        .toString(),
+  final albums = await albumModel.getAlbums();
+  final album = await albumModel.getAlbum(id: 1);
+  final postedAlbum = await albumModel.postAlbum(
+    body: <String, dynamic>{
+      'userId': 42,
+      'title': 'My posted album',
+    },
   );
-  debugPrint(
-    (await albumModel.updateAlbum(
-      id: 1,
-      body: <String, dynamic>{
-        'userId': 420,
-        'title': 'My updated album',
-      },
-    ))
-        .toString(),
+  final updatedAlbum = await albumModel.updateAlbum(
+    id: 1,
+    body: <String, dynamic>{
+      'userId': 99,
+      'title': 'My updated album',
+    },
   );
+
+  debugPrint(albums.toString());
+  debugPrint(album.toString());
+  debugPrint(postedAlbum.toString());
+  debugPrint(updatedAlbum.toString());
   await albumModel.deleteAlbum(id: 1);
 }
