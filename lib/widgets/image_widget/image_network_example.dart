00ae13df650dc26274bdc3d4a22f2ad6f3d06c8b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-09-09 16:19:07 +0900

    Update image_network_example.dart

--- lib/widgets/image_widget/image_network_example.dart
+++ lib/widgets/image_widget/image_network_example.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: Image.network(
-          'https://media1.tenor.com/m/NVP2kRD7CHsAAAAC/dancing-dog.gif',
-        ),
+      Image.network(
+        'https://media1.tenor.com/m/NVP2kRD7CHsAAAAC/dancing-dog.gif',
       ),
     );
