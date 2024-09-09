0f81e4ca24f7558524c68d3f78584e0b3fa4df2d
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-09-09 20:49:48 +0900

    Create download_and_save_text_file.dart

---
+++ lib/util/download_and_save_text_file.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+void main() => runApp(
+      const _MyStatelessWidget(),
+    );
+
+class _MyStatelessWidget extends StatelessWidget {
+  const _MyStatelessWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) async => _downloadAndSaveTextFile('https://www.w3.org/robots.txt'),
+    );
+    return const FlutterLogo();
+  }
+}
+
+Future<void> _downloadAndSaveTextFile(String url) async {
+  try {
+    final uri = Uri.parse(url);
+    final bytes = await _downloadTextFile(uri);
+    final path = await _saveTextFile(bytes, p.basename(uri.path));
+    debugPrint('👀adb pull $path ~/Desktop');
+  } on Exception catch (e, s) {
+    debugPrint(e.toString());
+    debugPrintStack(stackTrace: s);
+  }
+}
+
+Future<String> _downloadTextFile(Uri uri) async {
+  bool isSuccessful(int statusCode) => 200 <= statusCode && statusCode <= 299;
+
+  final response = await http.get(uri);
+  if (isSuccessful(response.statusCode)) {
+    return response.body;
+  }
+  throw Exception(
+    'Status code: ${response.statusCode}, Reason phrase: ${response.reasonPhrase}, URL: ${uri.path}',
+  );
+}
+
+Future<String> _saveTextFile(String contents, String filename) async {
+  final directory = await getExternalStorageDirectory();
+  if (directory == null) {
+    throw Exception('Failed to get the external storage directory.');
+  }
+  final path = p.join(directory.path, filename);
+  File(path).writeAsStringSync(contents, mode: FileMode.writeOnly);
+  return path;
+}
