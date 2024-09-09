d6fb5783c990913cc427a89d348640aa23d4ecd7
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-09-09 16:48:11 +0900

    Update http_example.dart

--- lib/packages/http_example.dart
+++ lib/packages/http_example.dart
@@ -14,10 +14,20 @@ class _MyStatelessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => FutureBuilder<Uint8List>(
         future: imageBytes,
-        builder: (_, snapshot) =>
-            snapshot.connectionState == ConnectionState.done
-                ? Image.memory(snapshot.data!)
-                : const CircularProgressIndicator(),
+        builder: (_, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const CircularProgressIndicator();
+          }
+          if (snapshot.hasError) {
+            debugPrint(snapshot.error.toString());
+            debugPrintStack(stackTrace: snapshot.stackTrace);
+            return const Icon(Icons.broken_image);
+          }
+          if (snapshot.hasData) {
+            return Image.memory(snapshot.data!);
+          }
+          return const SizedBox.shrink();
+        },
       );
 }
 
