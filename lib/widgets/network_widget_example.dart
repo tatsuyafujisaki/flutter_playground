250d764bd7bae33469321cc0ed0a7de79ff2b500
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-09-09 16:01:24 +0900

    Create network_widget_example.dart

---
+++ lib/widgets/network_widget_example.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Image.network(
+          'https://media1.tenor.com/m/NVP2kRD7CHsAAAAC/dancing-dog.gif',
+        ),
+      ),
+    );
