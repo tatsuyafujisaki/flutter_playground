7efbffd77e7c81763e440469d3a99e4795d59f37
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-09-10 21:37:59 +0900

    Update gal_example.dart

--- lib/packages/gal_example.dart
+++ lib/packages/gal_example.dart
@@ -3,15 +3,16 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_playground/packages/http_example.dart';
 import 'package:gal/gal.dart';
 
-Future<void> downloadToGallery(String url) async {
+Future<bool> downloadToGallery(String url) async {
   try {
-    if (!await Gal.requestAccess()) {
-      return;
+    if (await Gal.requestAccess()) {
+      final bytes = await downloadBytes(url);
+      await Gal.putImageBytes(bytes);
+      return true;
     }
-    final bytes = await downloadBytes(url);
-    await Gal.putImageBytes(bytes);
   } on Exception catch (e, s) {
     debugPrint(e.toString());
     debugPrintStack(stackTrace: s);
   }
+  return false;
 }
