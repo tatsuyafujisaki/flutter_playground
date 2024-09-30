e2b156fd18af803f06e7f553cc72eff5892d44a1
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-09-30 14:56:50 +0900

    Create firebase_on_background_message.dart and more

--- lib/packages/fcm/create_android_notification_channel.dart
+++ lib/packages/fcm/create_android_notification_channel.dart
@@ -1,22 +1,24 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-bool _isChannelCreated = false;
+FlutterLocalNotificationsPlugin? notificationsPlugin;
+const channel = AndroidNotificationChannel(
+  'dummy',
+  'dummy',
+  importance: Importance.high,
+);
 
 /// > Notification messages which arrive while the application is in the foreground will not display a visible notification by default, on both Android and iOS. It is, however, possible to override this behavior:
 /// On Android, you must create a "High Priority" notification channel.
 /// https://firebase.google.com/docs/cloud-messaging/flutter/receive#foreground_and_notification_messages
 Future<void> createAndroidNotificationChannel() async {
-  if (_isChannelCreated) {
+  if (notificationsPlugin != null) {
     return;
   }
   try {
-    const channel = AndroidNotificationChannel(
-      'My id',
-      'My name',
-      importance: Importance.high,
-    );
-    await FlutterLocalNotificationsPlugin()
+    notificationsPlugin = FlutterLocalNotificationsPlugin();
+    await notificationsPlugin!
         .resolvePlatformSpecificImplementation<
             AndroidFlutterLocalNotificationsPlugin>()
         ?.createNotificationChannel(channel);
@@ -24,5 +26,28 @@ Future<void> createAndroidNotificationChannel() async {
     debugPrint(e.toString());
     debugPrintStack(stackTrace: s);
   }
-  _isChannelCreated = true;
+}
+
+Future<void> showNotification(RemoteMessage message) async {
+  final notification = message.notification;
+  if (notification?.android == null) {
+    return;
+  }
+  try {
+    await notificationsPlugin?.show(
+      notification.hashCode,
+      notification!.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          icon: '@mipmap/ic_launcher',
+        ),
+      ),
+    );
+  } on Exception catch (e, s) {
+    debugPrint(e.toString());
+    debugPrintStack(stackTrace: s);
+  }
 }
--- lib/packages/fcm/firebase_message_handler.dart
+++ lib/packages/fcm/firebase_message_handler.dart
@@ -4,7 +4,8 @@ import 'dart:developer';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_playground/packages/fcm/firebase_background_message_handler.dart';
+import 'package:flutter_playground/packages/fcm/create_android_notification_channel.dart';
+import 'package:flutter_playground/packages/fcm/firebase_on_background_message.dart';
 
 class FirebaseMessageHandler {
   FirebaseMessageHandler() {
@@ -100,6 +101,7 @@ class FirebaseMessageHandler {
             '🔥FirebaseMessaging.onMessage received the following message.',
           );
           printMessage(message);
+          showNotification(message);
         },
       );
 
