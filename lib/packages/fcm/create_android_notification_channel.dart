1bd466ab4b740428113b5543eacccda60e2dd62b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-10-01 11:28:35 +0900

    Enritch create_android_notification_channel.dart

--- lib/packages/fcm/create_android_notification_channel.dart
+++ lib/packages/fcm/create_android_notification_channel.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_playground/packages/flutter_local_example.dart';
 
 FlutterLocalNotificationsPlugin? _notificationsPlugin;
 const _channel = AndroidNotificationChannel(
@@ -42,7 +43,10 @@ Future<void> showNotification(RemoteMessage message) async {
         android: AndroidNotificationDetails(
           _channel.id,
           _channel.name,
-          icon: '@mipmap/ic_launcher',
+          icon: '',
+          largeIcon: await createLargeIcon(
+            'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj_Jb2dSHvFPcUjxl753C-AkJDQdD71J9cwskYmrwpw2lcR7CoLEZU77s6ZWcgLsTJ_Rjsn2onNx1TkwlYv2_ziUm49HGN4fsMDccNN2HJBq3Wp-agn5U9Fc45FzDVKDJR81H4HYYF-zhE/s800/animal_inu.png',
+          ),
         ),
       ),
     );
