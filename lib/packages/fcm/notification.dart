d50444c38e3f033ae9f254912c5de58db1366739
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-10-01 22:29:34 +0900

    Update notification.dart

--- lib/packages/fcm/notification.dart
+++ lib/packages/fcm/notification.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,19 +9,14 @@ Future<void> showNotification(RemoteMessage message) async {
   if (notification == null) {
     return;
   }
-  try {
-    await _notificationsPlugin.show(
-      notification.hashCode,
-      notification.title,
-      notification.body,
-      await _createAndroidNotificationDetails(
-        'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj_Jb2dSHvFPcUjxl753C-AkJDQdD71J9cwskYmrwpw2lcR7CoLEZU77s6ZWcgLsTJ_Rjsn2onNx1TkwlYv2_ziUm49HGN4fsMDccNN2HJBq3Wp-agn5U9Fc45FzDVKDJR81H4HYYF-zhE/s800/animal_inu.png',
-      ),
-    );
-  } on Exception catch (e, s) {
-    debugPrint(e.toString());
-    debugPrintStack(stackTrace: s);
-  }
+  await _notificationsPlugin.show(
+    notification.hashCode,
+    notification.title,
+    notification.body,
+    await _createAndroidNotificationDetails(
+      'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj_Jb2dSHvFPcUjxl753C-AkJDQdD71J9cwskYmrwpw2lcR7CoLEZU77s6ZWcgLsTJ_Rjsn2onNx1TkwlYv2_ziUm49HGN4fsMDccNN2HJBq3Wp-agn5U9Fc45FzDVKDJR81H4HYYF-zhE/s800/animal_inu.png',
+    ),
+  );
 }
 
 Future<NotificationDetails> _createAndroidNotificationDetails(
