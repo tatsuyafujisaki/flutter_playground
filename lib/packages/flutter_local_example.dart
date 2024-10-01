ec28f6b8e997c9b269eb60aa017edec0ccbe2e3a
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-10-01 16:56:51 +0900

    Update flutter_local_example.dart

--- lib/packages/flutter_local_example.dart
+++ lib/packages/flutter_local_example.dart
@@ -1,11 +1,5 @@
-import 'dart:convert';
-import 'dart:typed_data';
-
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_playground/packages/http_example.dart';
 
 Future<ByteArrayAndroidBitmap> createLargeIcon(String url) async =>
-    _convertToBitmap(await downloadBytes(url));
-
-ByteArrayAndroidBitmap _convertToBitmap(Uint8List bytes) =>
-    ByteArrayAndroidBitmap.fromBase64String(base64Encode(bytes));
+    ByteArrayAndroidBitmap(await downloadBytes(url));
