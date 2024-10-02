438667b11b2da4e0027ea9ba9a86b7564eaa6a26
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-10-02 09:52:27 +0900

    Simplify main.dart and more

--- lib/main.dart
+++ lib/main.dart
@@ -12,36 +12,31 @@ import 'package:flutter_playground/shared_preferences/encypted_shared_preference
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
+  debugPrint('👀Platform version: ${Platform.version}');
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   PlatformDispatcher.instance.onError = (error, stackTrace) {
     FirebaseCrashlytics.instance.recordError(error, stackTrace);
     return true;
   };
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await EncryptedSharedPreferences.initialize();
   runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    debugPrint('👀Platform version: ${Platform.version}');
-
-    return MaterialApp(
-      home: const _MyStatefulWidget(title: 'Flutter Demo Home Page'),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-    );
-  }
+  Widget build(BuildContext context, WidgetRef ref) => MaterialApp(
+        home: const _MyStatefulWidget(title: 'My title'),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+      );
 }
 
 class _MyStatefulWidget extends StatefulWidget {
@@ -53,8 +48,7 @@ class _MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<_MyStatefulWidget> {
-  final myTextEditingController = TextEditingController(text: 'Initial text!');
-  int counter = 0;
+  final textEditingController = TextEditingController(text: 'My initial text');
   final firebaseMessageHandler = FirebaseMessageHandler();
 
   @override
@@ -71,54 +65,18 @@ class _MyStatefulWidgetState extends State<_MyStatefulWidget> {
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created
-        // by the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command
-          // in Visual Studio Code) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is
-          // the vertical axis because Columns are vertical
-          // (the cross axis would be horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(context.l10n.pushButtonCountLabel),
-            Text(
-              '$counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: const Center(
+        child: FlutterLogo(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
   @override
   void dispose() {
     firebaseMessageHandler.dispose();
-    myTextEditingController.dispose();
+    textEditingController.dispose();
     super.dispose();
-  }
-
-  void incrementCounter() {
-    setState(() => counter++);
   }
 }
