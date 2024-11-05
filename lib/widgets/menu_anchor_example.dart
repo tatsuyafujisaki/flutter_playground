9f78edadd1a486db3062ca201820579a852d9933
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-11-05 13:10:34 +0900

    Create menu_anchor_example.dart

---
+++ lib/widgets/menu_anchor_example.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            leading: _MyStatefulWidget(),
+          ),
+        ),
+      ),
+    );
+
+class _MyStatefulWidget extends StatefulWidget {
+  @override
+  State<_MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<_MyStatefulWidget> {
+  bool isMenuOpen = false;
+
+  @override
+  Widget build(BuildContext context) => MenuAnchor(
+        onOpen: () => setState(() => isMenuOpen = true),
+        onClose: () => setState(() => isMenuOpen = false),
+        menuChildren: <Widget>[
+          MenuItemButton(
+            onPressed: () async => _showDialog('🍎'),
+            child: const Text('🍎'),
+          ),
+          MenuItemButton(
+            onPressed: () async => _showDialog('🍊'),
+            child: const Text('🍊'),
+          ),
+        ],
+        builder: (_, controller, child) => TextButton(
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+          child: child ?? Icon(isMenuOpen ? Icons.menu_open : Icons.menu),
+        ),
+      );
+
+  Future<void> _showDialog(String title) async {
+    await showDialog<void>(
+      context: context,
+      builder: (_) => AlertDialog(title: Text(title)),
+    );
+  }
+}
