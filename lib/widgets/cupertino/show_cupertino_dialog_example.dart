bbde4c484c0096ed710147d93ea2cb088d66d94a
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-11-18 13:07:24 +0900

    Update show_cupertino_dialog_example.dart

--- lib/widgets/cupertino/show_cupertino_dialog_example.dart
+++ lib/widgets/cupertino/show_cupertino_dialog_example.dart
@@ -1,34 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(home: _MyStatelessWidget()));
+void main() => runApp(
+      const MaterialApp(
+        home: Scaffold(
+          body: _MyStatelessWidget(),
+        ),
+      ),
+    );
 
 class _MyStatelessWidget extends StatelessWidget {
   const _MyStatelessWidget();
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: IconButton(
-            onPressed: () async => showCupertinoDialog(
-              context: context,
-              builder: (context) => CupertinoAlertDialog(
-                title: const Text('Title'),
-                content: const Text('Content'),
-                actions: <Widget>[
-                  CupertinoDialogAction(
-                    onPressed: () => Navigator.pop(context),
-                    child: const Text('🍎'),
-                  ),
-                  CupertinoDialogAction(
-                    onPressed: () => Navigator.pop(context),
-                    child: const Text('🍊'),
-                  ),
-                ],
-              ),
+  Widget build(BuildContext context) => Center(
+        child: IconButton(
+          onPressed: () async => showCupertinoDialog(
+            context: context,
+            builder: (context) => CupertinoAlertDialog(
+              title: const Text('Title'),
+              content: const Text('Content'),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('🍎'),
+                ),
+                CupertinoDialogAction(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('🍊'),
+                ),
+              ],
             ),
-            icon: const Icon(Icons.flutter_dash),
           ),
+          icon: const Icon(Icons.flutter_dash),
         ),
       );
 }
