f65a3201248dc2a24880b5844cf69c4d61a429c0
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2024-12-24 13:37:22 +0900

    Update tutorial_coach_mark_example.dart

--- lib/widgets/packages/tutorial_coach_mark_example.dart
+++ lib/widgets/packages/tutorial_coach_mark_example.dart
@@ -8,7 +8,13 @@ void main() => runApp(
     );
 
 class _MyStatefulWidget extends StatefulWidget {
-  final keys = List.generate(3, (_) => GlobalKey());
+  final targetDataList = _createTargetDataList(
+    [
+      ('Title1', 'Body1'),
+      ('Title2', 'Body2'),
+      ('Title3', 'Body3'),
+    ],
+  );
 
   @override
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
@@ -20,14 +26,10 @@ class _MyStatefulWidgetState extends State<_MyStatefulWidget> {
   @override
   void initState() {
     super.initState();
-    tutorialCoachMark = TutorialCoachMark(
-      targets: createTargets(),
-      hideSkip: true,
-      focusAnimationDuration: Duration.zero,
-      unFocusAnimationDuration: Duration.zero,
-      pulseEnable: false,
-    );
-    tutorialCoachMark.show(context: context);
+    tutorialCoachMark = _createTutorialCoachMark(
+      widget.targetDataList,
+      () => tutorialCoachMark.next(),
+    )..show(context: context);
   }
 
   @override
@@ -37,57 +39,104 @@ class _MyStatefulWidgetState extends State<_MyStatefulWidget> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: List.generate(
               3,
-              (index) => Icon(key: widget.keys[index], Icons.flutter_dash),
+              (index) => Icon(
+                key: widget.targetDataList[index].$1,
+                Icons.flutter_dash,
+              ),
             ),
           ),
         ),
       );
+}
 
-  List<TargetFocus> createTargets() => List<TargetFocus>.generate(
-        3,
-        (index) => TargetFocus(
-          keyTarget: widget.keys[index],
-          contents: [
-            TargetContent(
-              child: ColoredBox(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: GestureDetector(
-                    onTap: () => tutorialCoachMark.next(),
-                    behavior: HitTestBehavior.translucent,
-                    child: const Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'ルームの作成・参加',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 8),
-                              Text(
-                                'ルームの作成・参加ができます。\n作成にはマネージャー以上の権限が必要です。',
-                                maxLines: 10,
-                                style: TextStyle(fontSize: 16),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ],
+List<(GlobalKey<State<StatefulWidget>>, String, String)> _createTargetDataList(
+  List<(String, String)> titleAndBodyList,
+) =>
+    titleAndBodyList
+        .map(
+          (titleAndBody) => (
+            GlobalKey<State<StatefulWidget>>(),
+            titleAndBody.$1,
+            titleAndBody.$2
+          ),
+        )
+        .toList();
+
+TutorialCoachMark _createTutorialCoachMark(
+  List<(GlobalKey<State<StatefulWidget>>, String, String)> targetDataList,
+  GestureTapCallback onTap,
+) =>
+    TutorialCoachMark(
+      targets: _createTargets(targetDataList, onTap),
+      paddingFocus: 0,
+      hideSkip: true,
+      focusAnimationDuration: Duration.zero,
+      unFocusAnimationDuration: Duration.zero,
+      pulseEnable: false,
+    );
+
+List<TargetFocus> _createTargets(
+  List<(GlobalKey<State<StatefulWidget>>, String, String)> tutorialDataList,
+  GestureTapCallback onTap,
+) =>
+    tutorialDataList
+        .map(
+          (data) => _createTarget(
+            keyTarget: data.$1,
+            title: data.$2,
+            body: data.$3,
+            onTap: onTap,
+          ),
+        )
+        .toList();
+
+TargetFocus _createTarget({
+  required GlobalKey<State<StatefulWidget>> keyTarget,
+  required String title,
+  required String body,
+  required GestureTapCallback onTap,
+}) =>
+    TargetFocus(
+      keyTarget: keyTarget,
+      contents: [
+        TargetContent(
+          child: ColoredBox(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: GestureDetector(
+                onTap: onTap,
+                behavior: HitTestBehavior.translucent,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            title,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        SizedBox(width: 8),
-                        Icon(Icons.arrow_circle_right, size: 50),
-                      ],
+                          const SizedBox(height: 8),
+                          Text(
+                            body,
+                            maxLines: 10,
+                            style: const TextStyle(fontSize: 16),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
+                    const SizedBox(width: 8),
+                    const Icon(Icons.arrow_circle_right, size: 50),
+                  ],
                 ),
               ),
             ),
-          ],
+          ),
         ),
-      );
-}
+      ],
+    );
