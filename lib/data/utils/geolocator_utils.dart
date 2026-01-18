84300a55564c97deec02e76d53a0a8b0405e34cd
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-01-18 19:21:54 +0900

    Update geolocator_utils.dart

--- lib/data/utils/geolocator_utils.dart
+++ lib/data/utils/geolocator_utils.dart
@@ -1,22 +1,20 @@
 import 'package:geolocator/geolocator.dart';
 
 Future<Position> getPosition() async {
-  LocationPermission permission;
-
   final locationServiceEnabled = await Geolocator.isLocationServiceEnabled();
   if (!locationServiceEnabled) {
     return Future.error('Location service is disabled.');
   }
 
-  permission = await Geolocator.checkPermission();
+  var permission = await Geolocator.checkPermission();
   if (permission == LocationPermission.denied) {
     permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      return Future.error('Location permission is denied.');
-    }
   }
 
-  return permission == LocationPermission.deniedForever
-      ? Future.error('Location permission is permanently denied.')
-      : Geolocator.getCurrentPosition();
+  if (permission == LocationPermission.denied ||
+      permission == LocationPermission.deniedForever) {
+    return Future.error('Location permission is denied.');
+  }
+
+  return Geolocator.getCurrentPosition();
 }
