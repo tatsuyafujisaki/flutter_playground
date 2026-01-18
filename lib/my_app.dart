87a57438043dc28579d7c1948654110692f61331
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-01-18 10:12:42 +0900

    Update my_app.dart

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,13 +1,13 @@
 import 'dart:async';
 import 'dart:developer' as developer;
 
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:upgrader/upgrader.dart';
 
 import 'l10n/app_localizations.dart';
 import 'packages/analytics/my_analytics_provider.dart';
-import 'packages/analytics/my_analytics_service.dart';
 import 'ui/page/google_maps_page.dart';
 import 'ui/provider/my_app_lifecycle_state.dart';
 
@@ -57,43 +57,11 @@ class _MyAppState extends ConsumerState<MyApp> {
           ),
         ],
       ),
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance),
+      ],
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      navigatorObservers: [
-        _AnalyticsNavigatorObserver(analyticsService: analyticsService),
-      ],
     );
-  }
-}
-
-class _AnalyticsNavigatorObserver extends NavigatorObserver {
-  _AnalyticsNavigatorObserver({required this.analyticsService});
-
-  final MyAnalyticsService analyticsService;
-
-  @override
-  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    super.didPush(route, previousRoute);
-    _logScreenView(route);
-  }
-
-  @override
-  void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {
-    super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
-    if (newRoute != null) {
-      _logScreenView(newRoute);
-    }
-  }
-
-  void _logScreenView(Route<dynamic> route) {
-    final settings = route.settings;
-    if (settings.name != null) {
-      unawaited(
-        analyticsService.logScreenView(
-          screenName: settings.name!,
-          screenClass: settings.name,
-        ),
-      );
-    }
   }
 }
