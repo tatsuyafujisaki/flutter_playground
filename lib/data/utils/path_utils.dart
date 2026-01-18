a2bff0cbb62dc0b42f34f914685d50462bbaafba
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-01-18 10:55:19 +0900

    Rename path_util.dart to path_utils.dart

--- lib/data/utils/path_utils.dart
+++ lib/data/utils/path_utils.dart
@@ -1,12 +1,19 @@
 import 'dart:developer' as developer;
 import 'dart:io';
 
-// > The path library was designed to be imported with a prefix,
-// > though you don't have to if you don't want to:
-// https://pub.dev/documentation/path/
+// > Though the path library was designed to be imported with a prefix,
+// > you don't have to if you don't want to.
+// https://pub.dev/packages/path
 import 'package:path/path.dart' as p;
 
-bool exists(String path) => File(path).existsSync();
+bool exists(String path) {
+  try {
+    return File(path).existsSync();
+  } on Exception catch (error, stackTrace) {
+    developer.log('', error: error, stackTrace: stackTrace);
+    return false;
+  }
+}
 
 String replaceExtension(String path, String extension) => p.setExtension(
   path,
