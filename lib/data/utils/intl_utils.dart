581be56e878da47214af657a55c26a221a616a09
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-01-18 10:57:19 +0900

    Update intl_utils.dart

--- lib/data/utils/intl_utils.dart
+++ lib/data/utils/intl_utils.dart
@@ -3,15 +3,13 @@ import 'dart:developer' as developer;
 import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
 
-class IntlUtils {
-  void printLocales() {
-    developer.log('👀Intl.defaultLocale: ${Intl.defaultLocale}');
-    developer.log('👀Intl.systemLocale: ${Intl.systemLocale}');
-    developer.log('👀Intl.getCurrentLocale(): ${Intl.getCurrentLocale()}');
+void printLocales() {
+  developer.log('👀Intl.defaultLocale: ${Intl.defaultLocale}');
+  developer.log('👀Intl.systemLocale: ${Intl.systemLocale}');
+  developer.log('👀Intl.getCurrentLocale(): ${Intl.getCurrentLocale()}');
 
-    final locale = PlatformDispatcher.instance.locale;
-    developer.log('👀Locale: $locale');
-    developer.log('👀Locale.countryCode: ${locale.countryCode}');
-    developer.log('👀Locale.languageCode: ${locale.languageCode}');
-  }
+  final locale = PlatformDispatcher.instance.locale;
+  developer.log('👀Locale: $locale');
+  developer.log('👀Locale.countryCode: ${locale.countryCode}');
+  developer.log('👀Locale.languageCode: ${locale.languageCode}');
 }
