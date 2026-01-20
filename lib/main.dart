35b48a8b2f876f8312e614b17dd7ebf52b969d2b
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-01-21 08:02:10 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer' as developer;
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,6 +10,14 @@ import 'my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  developer.log(
+    '👀GOOGLE_MAPS_PLATFORM_API_KEY: '
+    '${const String.fromEnvironment('GOOGLE_MAPS_PLATFORM_API_KEY')}',
+  );
+  developer.log(
+    '👀YOUTUBE_API_KEY: ${const String.fromEnvironment('YOUTUBE_API_KEY')}',
+  );
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
