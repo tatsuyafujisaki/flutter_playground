d6bdac746a1b1f6b7f31fbeecb09a02b40530848
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-01-24 09:34:42 +0900

    Update upgrade_utils.dart

--- lib/data/utils/upgrade_utils.dart
+++ lib/data/utils/upgrade_utils.dart
@@ -1,17 +1,28 @@
+import 'dart:developer' as developer;
 import 'dart:io';
 
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:pub_semver/pub_semver.dart';
 
-Future<bool> shouldForceUpdate(String requiredVersion) async {
-  final info = await PackageInfo.fromPlatform();
-  return Version.parse(info.version) < Version.parse(requiredVersion);
+Future<bool> _shouldForceUpdate(String requiredVersion) async {
+  final packageInfo = await PackageInfo.fromPlatform();
+  final currentVersion = _tryParseVersion(packageInfo.version) ?? Version.none;
+  final requiredVersion1 = _tryParseVersion(requiredVersion) ?? Version.none;
+  return currentVersion < requiredVersion1;
+}
+
+Version? _tryParseVersion(String version) {
+  try {
+    return Version.parse(version);
+  } on Exception catch (error, stackTrace) {
+    developer.log('', error: error, stackTrace: stackTrace);
+    return null;
+  }
 }
 
 Future<String?> _getStoreUrl() async {
   if (Platform.isIOS) {
-    // YouTube as a dummy
-    const appStoreId = 'id544007664';
+    const appStoreId = 'id544007664'; // dummy
     return 'https://apps.apple.com/app/id$appStoreId';
   }
   if (Platform.isAndroid) {
