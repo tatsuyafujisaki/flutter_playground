14375da36bf3a6d885931cfc05119c67c47f1143
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-02-15 14:15:00 +0900

    Refactor: Consolidate utility files into `others` and relocate Future/Stream Builder examples to `ui/example` directories.

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,31 +1,22 @@
-import 'dart:developer' as developer;
+import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'core/error_handling.dart';
 import 'data/utils/coffee_log_utils.dart';
 import 'my_app.dart';
+import 'others/error_handling.dart';
 
 Future<void> mainCommon(FirebaseOptions firebaseOptions) async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  developer.log(
-    '👀GOOGLE_MAPS_PLATFORM_API_KEY: '
-    '${const String.fromEnvironment('GOOGLE_MAPS_PLATFORM_API_KEY')}',
-  );
-  developer.log(
-    '👀YOUTUBE_API_KEY: ${const String.fromEnvironment('YOUTUBE_API_KEY')}',
-  );
 
   await Firebase.initializeApp(options: firebaseOptions);
 
   setupErrorHandling();
 
   final container = ProviderContainer();
-  // ignore: unawaited_futures
-  logNearbyCoffeeChains(container);
+  unawaited(logNearbyCoffeeChains(container));
 
   runApp(UncontrolledProviderScope(container: container, child: const MyApp()));
 }
