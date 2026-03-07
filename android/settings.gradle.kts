52b2ff36f5e38b4ebc003a061660b11456320830
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-03-07 17:58:42 +0900

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -21,11 +21,12 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
-    id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
 }
 
 include(":app")
