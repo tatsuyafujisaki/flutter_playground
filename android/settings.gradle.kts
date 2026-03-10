82753a0417b497d57c3cd7044e570b9324df0e65
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2026-03-10 09:09:32 +0900

    build: Update Android Gradle Plugin to version 9.1.0.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -21,7 +21,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.android.application") version "9.0.1" apply false
+    id("com.android.application") version "9.1.0" apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
